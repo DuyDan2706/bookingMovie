c20a1f1e1b87abea78a5934f95569ff8ed464c70
DuyDan2706 <duydan272000@gmail.com>
2022-09-10 22:27:52 +0700

    first commit

---
+++ src/redux/configStore.js
@@ -0,0 +1,20 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import thunk from 'redux-thunk'
+import { CarouselReducer } from './reducers/CarouselReducer';
+import { QuanLyNguoiDungReducer } from './reducers/QuanLyNguoiDungReducer';
+import { QuanLyPhimReducer } from './reducers/QuanLyPhimReducer';
+import { QuanLyRapReducer } from './reducers/QuanLyRapReducer';
+
+const rootReducer = combineReducers({
+    //state ứng dụng
+    CarouselReducer,
+    QuanLyPhimReducer,
+    QuanLyRapReducer,
+    QuanLyNguoiDungReducer
+});
+
+
+export const store = createStore(rootReducer,applyMiddleware(thunk));
+
+
+
